0b2736de656f833abaebefffda419afb60b88872
waleolajumoke <waleolajumoke@gmail.com>
2026-07-25 11:17:07 +0100

    pushing to github

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Node.js application deployed automatically with Jenkins!");
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "healthy",
+    application: "nodejs-cicd-app",
+  });
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Application is running on port ${PORT}`);
+});
