dca92785daf9bd21a6429820206cb843d39775ba
collinsagho <collinsaimuanvbosa@gmail.com>
2026-07-25 15:03:53 +0300

    modified app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("welcome to tech365!");
+  res.send("welcome to tech365, thanks!");
 });
 
 app.get("/health", (req, res) => {
