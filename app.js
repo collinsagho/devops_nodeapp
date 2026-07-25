9f3e7e04dfa5044f93cafeb5a55b3d8e7a24d666
collinsagho <collinsaimuanvbosa@gmail.com>
2026-07-25 16:40:34 +0300

    edited jenkins

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("welcome to tech365, thanks you sir! I am grateful to you for your support and guidance. I will keep learning and improving my skills. Collins");
+  res.send("welcome to tech365, thanks you sir! I am grateful to you for your support and guidance. I will keep learning and improving my skills. Collins learn a lot today");
 });
 
 app.get("/health", (req, res) => {
